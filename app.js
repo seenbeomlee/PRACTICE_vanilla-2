d238cfdafafe485708bc7796426cc5d932b3c4d8
Seenbeom Lee <mail@seenblee.com>
2020-03-18 01:11:59 +0900

    Vanilla-2 2.6
    Vanilla-2 2.6 / Saving the Image

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "#2c2c2c";
 
 canvas.width = 700;
 canvas.height = 700;
 
+// Default back-ground
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+// Default back-ground
+
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
+
 ctx.lineWidth = 2.5;
 
 // ctx.fillStyle = "blue";
@@ -57,20 +64,32 @@ function handleRangeChange(event) {
 }
 
 function handleModeClick(event) {
-  if (filling) {
-    filling = false;
+  if (!filling) {
+    filling = true;
     mode.innerText = "Fill";
   } else {
-    filling = true;
+    filling = false;
     mode.innerText = "Paint";
   }
 }
 
 function handleCanvasClick(event) {
-  if (!filling) {
+  if (filling) {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
   } else {
   }
+}
+
+function handleSaveClick() {
+  const image = canvas.toDataURL();
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "PaintJS[EXPORT]";
+  link.click();
+}
+
+function handleRightClick(event) {
+  event.preventDefault();
 }
 
 if (canvas) {
@@ -79,6 +98,7 @@ if (canvas) {
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
   canvas.addEventListener("click", handleCanvasClick);
+  canvas.addEventListener("contextmenu", handleRightClick);
 }
 
 Array.from(colors).forEach(color =>
@@ -91,4 +111,8 @@ if (range) {
 
 if (mode) {
   mode.addEventListener("click", handleModeClick);
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener("click", handleSaveClick);
 }
